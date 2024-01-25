d0ae1a3ddee2c4854e5b4f9d83bc252384ad98b6
Clara Silvia Anggreini <clara.silvia6979@gmail.com>
2024-01-25 14:22:33 +0700

    CV

---
+++ silvia.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("button").click(function () {
+    $(".div1").slideToggle(1000);
+  });
+});
+$(document).ready(function () {
+  $("#btnsd").click(function () {
+    $(".sd").slideToggle(1000);
+  });
+});
+$(document).ready(function () {
+  $("#btnsmp").click(function () {
+    $(".smp").slideToggle(1000);
+  });
+});
+$(document).ready(function () {
+  $("#btnsma").click(function () {
+    $(".sma").slideToggle(1000);
+  });
+});
+$(document).ready(function () {
+  $("#btnkuliah").click(function () {
+    $(".kuliah").slideToggle(1000);
+  });
+});
